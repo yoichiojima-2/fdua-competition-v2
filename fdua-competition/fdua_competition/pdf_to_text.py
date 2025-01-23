79f739bfff92e59520e20fa58660139391c9f981
Yoichi Ojima <yoichiojima@gmail.com>
2025-01-24 00:51:28 +0900

    refactor: read_pdf -> pdf_to_text

--- fdua-competition/fdua_competition/pdf_to_text.py
+++ fdua-competition/fdua_competition/pdf_to_text.py
@@ -3,17 +3,17 @@ import pypdfium2 as pdfium
 from tqdm import tqdm
 
 
-def get_documents_dir():
+def get_documents_dir() -> Path:
     return Path().cwd().parent / "downloads/documents"
 
 
-def get_interim_dir():
+def get_interim_dir() -> Path:
     dir = Path().cwd().parent / "interim"
     dir.mkdir(exist_ok=True)
     return dir
 
 
-def read_pdf(filename: str):
+def pdf_to_text(filename: str) -> None:
     input_path = get_documents_dir() / filename
     pdf = pdfium.PdfDocument(input_path)
 
@@ -21,12 +21,12 @@ def read_pdf(filename: str):
         text = page.get_textpage().get_text_bounded().replace("\n", "")
         (get_interim_dir() / f"{input_path.stem}_{index}.txt").write_text(text)
 
-    print(f"[read_pdf] done reading {filename}\n")
+    print(f"[pdf_to_text] done: {input_path}")
 
 
 def main():
     for pdf_file in tqdm(get_documents_dir().glob("*.pdf")):
-        read_pdf(pdf_file.name)
+        pdf_to_text(pdf_file.name)
 
 
 if __name__ == "__main__":
