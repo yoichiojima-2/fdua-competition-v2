9184a08be506d9f015572c9e259ae17e7f4aacbf
Yoichi Ojima <yoichiojima@gmail.com>
2025-01-26 00:23:04 +0900

    refactor: test_vectorize.py

--- tests/test_vectorize.py
+++ tests/test_vectorize.py
@@ -3,5 +3,4 @@ from fdua_competition.vectorize import vectorize_text
 
 def test_vectorize_text():
     result = vectorize_text("test")
-    assert result
     assert all(isinstance(x, float) for x in result)
