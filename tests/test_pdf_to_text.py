340befa75e74a4ce73dae8d56b430579889bb90d
Yoichi Ojima <yoichiojima@gmail.com>
2025-01-25 23:07:08 +0900

    test: add test for pdf_to_text

---
+++ tests/test_pdf_to_text.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from fdua_competition.pdf_to_text import pdf_to_text
+from fdua_competition.utils import get_interim_dir
+
+
+def test_pdf_to_text():
+    (get_interim_dir() / "1_0.txt").unlink(missing_ok=True)
+    pdf_to_text("1.pdf")
+    assert (get_interim_dir() / "1_0.txt").exists()
