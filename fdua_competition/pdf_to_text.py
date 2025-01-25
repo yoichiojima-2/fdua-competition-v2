b2e341038f215ddcd1acd3ced854c249ed7345df
Yoichi Ojima <yoichiojima@gmail.com>
2025-01-25 23:06:33 +0900

    refactor: fix print (pdf_to_text)

--- fdua_competition/pdf_to_text.py
+++ fdua_competition/pdf_to_text.py
@@ -12,13 +12,15 @@ def pdf_to_text(filename: str) -> None:
     input_path = get_documents_dir() / filename
     pdf = pdfium.PdfDocument(input_path)
 
+    print(f"[pdf_to_text] processing: {input_path}")
+
     for index, page in tqdm(enumerate(pdf)):
         output_path = get_interim_dir() / f"{input_path.stem}_{index}.txt"
         text = page.get_textpage().get_text_bounded().replace("\n", "")
         output_path.write_text(text)
+        print(f"[pdf_to_text] exported: {output_path}")
 
     print("[pdf_to_text] done")
-    print(f"source: {input_path}\noutput: {output_path}")
 
 
 def main():
