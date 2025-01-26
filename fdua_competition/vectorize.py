7224a75da5127ad911ba47a15e44f5c203d2eb83
yo <yoichiojima@gmail.com>
2025-01-26 19:22:42 +0900

    refactor: simplify vectorize.py

--- fdua_competition/vectorize.py
+++ fdua_competition/vectorize.py
@@ -1,17 +1,9 @@
-import os
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 
 
 def vectorize_text(text: str) -> list[str]:
     load_dotenv()
-
-    client = AzureOpenAI(
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT_URL"),
-        api_version=os.getenv("AZURE_OPENAI_VERSION"),
-    )
-
+    client = AzureOpenAI()
     res = client.embeddings.create(input=text, model="embedding")
-
     return res.data[0].embedding
