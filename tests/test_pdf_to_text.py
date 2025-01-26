c68e223bc19ffde840ea6199135b1ab3eed93702
Yoichi Ojima <yoichiojima@gmail.com>
2025-01-26 21:59:49 +0900

    test: cleanup test

--- tests/test_pdf_to_text.py
+++
@@ -1,9 +0,0 @@
-from fdua_competition.pdf_to_text import pdf_to_text
-from fdua_competition.utils import get_interim_dir
-
-
-def test_pdf_to_text():
-    target = get_interim_dir() / "1_0.txt"
-    target.unlink(missing_ok=True)
-    pdf_to_text("1.pdf")
-    assert target.exists()
--- tests/test_vectorize.py
+++
@@ -1,6 +0,0 @@
-from fdua_competition.vectorize import vectorize_text
-
-
-def test_vectorize_text():
-    vector = vectorize_text("test")
-    assert all(isinstance(x, float) for x in vector)
