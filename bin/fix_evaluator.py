adc727fa0a3b4f433310310eae194698a802acd8
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-02 02:10:58 +0900

    style: misc change

--- bin/fix_evaluator.py
+++ bin/fix_evaluator.py
@@ -8,23 +8,23 @@ from main import get_root
 
 def main() -> None:
     path = get_root() / "evaluation/src/evaluator.py"
-    original_code = path.read_text()
-    modified_code = "\n".join(
-        [
-            "from pathlib import Path",
-            "from dotenv import load_dotenv",
-            "",
-            "",
-            "load_dotenv(Path(__file__).parent.parent.parent.parent / 'secrets/.env')",
-            "",
-            (
-                original_code.replace("from openai import OpenAI", "from openai import AzureOpenAI").replace(
-                    "OpenAI()", "AzureOpenAI()"
-                )
-            ),
-        ]
+    path.write_text(
+        "\n".join(
+            [
+                "from pathlib import Path",
+                "from dotenv import load_dotenv",
+                "",
+                "",
+                "load_dotenv(Path(__file__).parent.parent.parent.parent / 'secrets/.env')",
+                "",
+                (
+                    path.read_text()
+                    .replace("from openai import OpenAI", "from openai import AzureOpenAI")
+                    .replace("OpenAI()", "AzureOpenAI()")
+                ),
+            ]
+        )
     )
-    path.write_text(modified_code)
 
 
 if __name__ == "__main__":
