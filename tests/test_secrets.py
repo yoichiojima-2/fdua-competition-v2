4632f04a8b54def1aa0d1f2509f378387f94a99e
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-04 22:18:35 +0900

    test: add assets / secrets test

---
+++ tests/test_secrets.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+
+def test_assets():
+    secrets_dir = Path(__file__).parent.parent / "secrets"
+    existing_items = sorted([i for i in secrets_dir.iterdir()])
+    expected_items = sorted([
+        secrets_dir / i for i in [
+            ".env",
+            "google-application-credentials.json"
+        ]
+    ])
+    assert existing_items == expected_items
