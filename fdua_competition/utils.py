811b8021202740d180697ac8edcb966c8e2c6dd1
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-06 16:01:52 +0900

    fix: csv writer is now able to write pydantic BaseModel

--- fdua_competition/utils.py
+++ fdua_competition/utils.py
@@ -25,12 +25,14 @@ def get_queries(mode: Mode) -> list[str]:
 
 
 def print_before_retry(retry_state):
-    print(f":( retrying attempt {retry_state.attempt_number} after exception: {retry_state.outcome.exception()}", file=sys.stderr)
+    print(
+        f":( retrying attempt {retry_state.attempt_number} after exception: {retry_state.outcome.exception()}", file=sys.stderr
+    )
 
 
 def write_result(output_name: str, responses: list[ChatResponse]) -> None:
     output_path = get_root() / f"results/{output_name}.csv"
     output_path.parent.mkdir(parents=True, exist_ok=True)
-    df = pd.DataFrame([{"response": res["response"]} for res in responses])
+    df = pd.DataFrame([{"response": res.response} for res in responses])
     df.to_csv(output_path, header=False)
     print(f"[write_result] done: {output_path}")
