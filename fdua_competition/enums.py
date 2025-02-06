c43ca1fb07fae50fb2fb776c5bf27a8f54da1976
yo <145314115+yoichiojima-2@users.noreply.github.com>
2025-02-07 01:34:32 +0900

    fix: enums.py

--- fdua_competition/enums.py
+++ fdua_competition/enums.py
@@ -8,12 +8,10 @@ from enum import Enum
 class Mode(Enum):
     """
     動作モードを表すenum
-
     attributes:
         SUBMIT: 提出用モード
         TEST: テスト用モード
     """
-
     SUBMIT = "submit"  # 提出用モード
     TEST = "test"  # テスト用モード
 
@@ -21,33 +19,27 @@ class Mode(Enum):
 class ChatModelOption(Enum):
     """
     chatモデルのオプションを表すenum
-
     attributes:
         AZURE: Azure のchatモデルを使用
     """
-
     AZURE = "azure"
 
 
 class EmbeddingModelOption(Enum):
     """
     embeddingモデルのオプションを表すenum.
-
     attributes:
         AZURE: Azure のembeddingモデルを使用
     """
-
     AZURE = "azure"
 
 
 class VectorStoreOption(Enum):
     """
     vectorstoreのオプションを表すenum.
-
     attributes:
         CHROMA: Chroma vectorstore
         IN_MEMORY: インメモリのvectorstore
     """
-
     CHROMA = "chroma"
     IN_MEMORY = "in-memory"
