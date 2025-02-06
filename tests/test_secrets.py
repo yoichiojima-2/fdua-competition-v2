228cb9218fc66a721ff968efae09fbe2292b960a
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-06 17:51:39 +0900

    test: fix test_secrets

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -4,5 +4,5 @@ from pathlib import Path
 def test_assets():
     secrets_dir = Path(__file__).parent.parent / "secrets"
     existing_items = sorted([i for i in secrets_dir.iterdir()])
-    expected_items = sorted([secrets_dir / i for i in [".success", ".env", "google-application-credentials.json"]])
+    expected_items = sorted([secrets_dir / i for i in [".env", "google-application-credentials.json"]])
     assert existing_items == expected_items
