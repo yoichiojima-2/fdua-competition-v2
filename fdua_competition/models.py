3058704a4c36c2c407e1cf2369182b98d07a6a7c
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-08 21:55:27 +0900

    fix: embedddings -> models

---
+++ fdua_competition/models.py
@@ -0,0 +1,21 @@
+from langchain_core.embeddings import Embeddings
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+
+from fdua_competition.enums import ChatOpt, EmbeddingOpt
+
+
+def create_embeddings(opt: EmbeddingOpt = EmbeddingOpt.AZURE) -> Embeddings:
+    match opt:
+        case EmbeddingOpt.AZURE:
+            return AzureOpenAIEmbeddings(azure_deployment="embedding")
+        case _:
+            raise ValueError("Invalid embedding option")
+
+
+def create_chat_model(opt: ChatOpt = ChatOpt.AZURE) -> BaseChatModel:
+    match opt:
+        case ChatOpt.AZURE:
+            return AzureChatOpenAI(azure_deployment="4omini")
+        case _:
+            raise ValueError("Invalid chat option")
