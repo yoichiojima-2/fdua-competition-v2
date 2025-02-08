21d8065b58a89ff9adeb6145c680f96210ba0643
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-08 21:56:55 +0900

    feat: add read_queries / write_result

--- fdua_competition/utils.py
+++ fdua_competition/utils.py
@@ -1,6 +1,35 @@
+import os
+from pathlib import Path
 from pprint import pprint
 
+import pandas as pd
+from pydantic import BaseModel
 from tenacity import RetryCallState
+
+from fdua_competition.enums import Mode
+
+
+def read_queries(mode: Mode) -> list[str]:
+    match mode:
+        case Mode.TEST:
+            df = pd.read_csv(Path(os.environ["FDUA_DIR"]) / ".fdua-competition/validation/ans_txt.csv")
+            return df["problem"].tolist()
+        case Mode.SUBMIT:
+            df = pd.read_csv(Path(os.environ["FDUA_DIR"]) / ".fdua-competition/query.csv")
+            return df["problem"].tolist()
+        case _:
+            raise ValueError(f"): unknown mode: {mode}")
+
+
+def write_result(output_name: str, responses: list[BaseModel]) -> None:
+    assert responses[0].response, "response field is missing"
+    output_path = os.environ["FDUA_DIR"] / f".fdua-competition/results/{output_name}.csv"
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    df = pd.DataFrame([{"response": res.response} for res in responses])
+    df.to_csv(output_path, header=False)
+
+    print(f"[write_result] done: {output_path}")
 
 
 def log_retry(state: RetryCallState) -> None:
