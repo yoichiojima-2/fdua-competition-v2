431dd6d4c578735a78cd3b55085d95938d1c47fc
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-10 00:12:36 +0900

    perf: concurrently run main

--- fdua_competition/main.py
+++ fdua_competition/main.py
@@ -7,6 +7,7 @@ from fdua_competition.enums import EmbeddingOpt, Mode
 from fdua_competition.models import create_embeddings
 from fdua_competition.utils import read_queries, write_result
 from fdua_competition.vectorstore import FduaVectorStore
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def parse_args() -> Namespace:
@@ -16,18 +17,29 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
+def process_queries_concurrently(queries: list[str], vectorstore: FduaVectorStore) -> list[str]:
+    responses = []
+    with ThreadPoolExecutor() as executor:
+        future_to_query = {executor.submit(answer_query, query=query, vectorstore=vectorstore): query for query in queries}
+        for future in tqdm(as_completed(future_to_query), total=len(queries), desc="processing queries.."):
+            query = future_to_query[future]
+            try:
+                response = future.result()
+                print(response)
+                responses.append(response)
+            except Exception as exc:
+                print(f"[process_queries_concurrently] {query} generated an exception: {exc}")
+    return responses
+
+
 def main():
     args = parse_args()
 
     embeddings = create_embeddings(EmbeddingOpt.AZURE)
     vs = FduaVectorStore(embeddings)
 
-    responses = []
-    for query in tqdm(read_queries(Mode(args.mode))):
-        response = answer_query(query=query, vectorstore=vs)
-        print(response)
-        responses.append(response)
-
+    queries = read_queries(Mode(args.mode))
+    responses = process_queries_concurrently(queries, vs)
     write_result(responses=responses)
     print("[main] :)  done")
 
