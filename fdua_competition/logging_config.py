d5553ed406c839bbcd209f9d35365943aad6e7ba
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-10 16:24:45 +0900

    feat: add file handler to logger

--- fdua_competition/logging_config.py
+++ fdua_competition/logging_config.py
@@ -1,17 +1,30 @@
 import logging
+import os
 from logging import Logger
+from pathlib import Path
 
 from fdua_competition.get_version import get_version
+
+LOG_DIR = Path(os.environ["FDUA_DIR"]) / ".fdua-competition/logs"
 
 
 def get_logger() -> Logger:
     logger = logging.getLogger(f"v{get_version()}")
+
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+
     if not logger.hasHandlers():
         handler = logging.StreamHandler()
         formatter = logging.Formatter("%(levelname)s: %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(logging.INFO)
+
+        file_handler = logging.FileHandler(LOG_DIR / f"{get_version()}.log")
+        file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
     return logger
 
 
