30eb5b77132c138b39fe538e83ac6d08498d2d75
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-10 13:35:26 +0900

    fix: vectorstore

--- fdua_competition/vectorstore.py
+++ fdua_competition/vectorstore.py
@@ -7,28 +7,18 @@ from pathlib import Path
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
-from langchain_core.vectorstores import VectorStore
 from langchain_core.vectorstores.base import VectorStoreRetriever
-from tenacity import retry, stop_after_attempt, wait_fixed
+from tenacity import retry, stop_after_attempt, wait_random
 from tqdm import tqdm
 
 from fdua_competition.enums import EmbeddingOpt
 from fdua_competition.get_version import get_version
-from fdua_competition.logger import get_logger
+from fdua_competition.logging_config import logger
 from fdua_competition.models import create_embeddings
 from fdua_competition.pdf_handler import load_documents
 from fdua_competition.utils import before_sleep_hook
 
 BATCH_SIZE = 4
-logger = get_logger()
-
-
-def add_documents_concurrently(vectorstore: VectorStore, docs: list[Document]) -> None:
-    batches = [docs[i : i + BATCH_SIZE] for i in range(0, len(docs), BATCH_SIZE)]
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(vectorstore.add, batch) for batch in batches]
-        for future in tqdm(as_completed(futures), total=len(futures), desc="populating vectorstore.."):
-            future.result()
 
 
 class FduaVectorStore:
@@ -50,14 +40,22 @@ class FduaVectorStore:
     def as_retriever(self, **kwargs) -> VectorStoreRetriever:
         return self.vectorstore.as_retriever(**kwargs)
 
-    @retry(stop=stop_after_attempt(24), wait=wait_fixed(1), before_sleep=before_sleep_hook)
+    @retry(stop=stop_after_attempt(24), wait=wait_random(min=2, max=10), before_sleep=before_sleep_hook)
     def add(self, docs: list[Document]) -> None:
         self.vectorstore.add_documents(docs)
+
+    def add_documents_concurrently(self, docs: list[Document]) -> None:
+        batches = [docs[i : i + BATCH_SIZE] for i in range(0, len(docs), BATCH_SIZE)]
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.add, batch) for batch in batches]
+            for future in tqdm(as_completed(futures), total=len(futures), desc="populating vectorstore.."):
+                future.result()
 
     def populate(self) -> None:
         self.vectorstore.reset_collection()
         docs = load_documents()
-        add_documents_concurrently(self, docs)
+        self.add_documents_concurrently(docs)
+        logger.info("[FduaVectorStore] done populating vectorstore")
 
 
 def parse_args() -> Namespace:
